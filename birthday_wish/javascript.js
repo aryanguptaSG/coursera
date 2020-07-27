0e4e960c869173ff2d1e4e39e90c7ff47d1b85c1
Gouri Shanker <shanker.gouri@hotmail.com>
2020-07-27 20:48:54 +0530

    Update javascript.js

--- birthday_wish/javascript.js
+++ birthday_wish/javascript.js
@@ -3,10 +3,13 @@ function fn(){
     document.getElementById("first").style.display="none";
     document.getElementById("second").style.display="none";
         document.getElementById("aryan").style.display="none";
+
     document.getElementById("response").style.display = "block";
+      document.documentElement.scrollTop=0;
     document.getElementById("rname").innerHTML="welcome "+" ' "+n+" ' "+" To my Website";
     document.getElementById("rwish1").innerHTML="I Wish You A Very <br><br>Happy Birthday "+" <br><br>' "+n+" ' ";
     document.getElementById("rwish2").innerHTML="May God <br>Bless You<br>"+" ' "+n+" '";
+
 
 
 
