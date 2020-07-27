6ab168dc1ed96297eb9b499a61d9fc1f1bf38dff
aryanguptaSG <68845364+aryanguptaSG@users.noreply.github.com>
2020-07-27 16:15:01 +0530

    Add files via upload

--- birthday_wish/javascript.js
+++ birthday_wish/javascript.js
@@ -2,7 +2,12 @@ function fn(){
     var n = document.getElementById("username").value;
     document.getElementById("first").style.display="none";
     document.getElementById("second").style.display="none";
+        document.getElementById("aryan").style.display="none";
     document.getElementById("response").style.display = "block";
-    document.getElementById("rname").innerHTML="welcome "+n+" To my Website";
+    document.getElementById("rname").innerHTML="welcome "+" ' "+n+" ' "+" To my Website";
+    document.getElementById("rwish1").innerHTML="I Wish You A Very <br><br>Happy Birthday "+" <br><br>' "+n+" ' ";
+    document.getElementById("rwish2").innerHTML="May God <br>Bless You<br>"+" ' "+n+" '";
+
+
 
 }
